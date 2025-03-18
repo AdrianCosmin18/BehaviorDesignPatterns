88efa985f6fff9faa5865b6863d1f1afc68a5a6d
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2025-03-18 21:30:50 +0200

    create macro command to switch off all lights in the house

---
+++ src/main/java/org/example/command/switchExercise/AllLightsCommand.java
@@ -0,0 +1,21 @@
+package org.example.command.switchExercise;
+
+import java.util.List;
+
+public class AllLightsCommand implements Command {
+    private final List<Light> lights;
+
+
+    public AllLightsCommand(List<Light> lights) {
+        this.lights = lights;
+    }
+
+    @Override
+    public void execute() {
+        lights.forEach(light -> {
+            if (light.isOn()) {
+                light.toggle();
+            }
+        });
+    }
+}
--- src/main/java/org/example/command/switchExercise/CommandDemo.java
+++ src/main/java/org/example/command/switchExercise/CommandDemo.java
@@ -1,14 +1,27 @@
 package org.example.command.switchExercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandDemo {
     public static void main(String[] args) {
-        Light light = new Light();
+        Light bedroomLight = new Light();
+        Light kitchenLight = new Light();
         Switch lightSwitch = new Switch();
 
-        Command toggleCommand = new ToggleCommand(light);
+        Command toggleCommand1 = new ToggleCommand(bedroomLight);
+        Command toggleCommand2 = new ToggleCommand(kitchenLight);
 
-        lightSwitch.storeAndExecute(toggleCommand);
-        lightSwitch.storeAndExecute(toggleCommand);
-        lightSwitch.storeAndExecute(toggleCommand);
+        lightSwitch.storeAndExecute(toggleCommand1);
+        lightSwitch.storeAndExecute(toggleCommand2);
+        lightSwitch.storeAndExecute(toggleCommand1);
+
+        List<Light> lights = new ArrayList<Light>();
+        lights.add(bedroomLight);
+        lights.add(kitchenLight);
+
+        Command allLightsCommand = new AllLightsCommand(lights);
+        lightSwitch.storeAndExecute(allLightsCommand);
+
     }
 }
